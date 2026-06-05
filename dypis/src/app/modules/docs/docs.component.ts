0c501e93b80219f9c822c3661cc55f8a470b9a33
NaveenSohal <naveens@theninjastudio.com>
2026-06-05 15:44:04 +0530

    feat: cbse docs

--- dypis/src/app/modules/docs/docs.component.ts
+++ dypis/src/app/modules/docs/docs.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
+import { Doc } from 'src/app/models/interfaces';
 import { DocsService } from './docs.service';
 import { updateAnimationStateOnScroll } from 'src/app/utils/helpers';
 
@@ -14,9 +14,130 @@ import { updateAnimationStateOnScroll } from 'src/app/utils/helpers';
 export class DocsComponent {
   state = 'start';
   updateAnimationStateOnScroll = updateAnimationStateOnScroll;
+
   docs$ = this.docsService.docs$;
+
   faSearch = faSearch;
   docsFilterCtrl = new FormControl<string>('');
+
+  generalInformation = [
+    { id: 1, label: 'Name of the School', value: '' },
+    { id: 2, label: 'Affiliation Number (if applicable)', value: '' },
+    { id: 3, label: 'School Code (if applicable)', value: '' },
+    { id: 4, label: 'Complete Address with PIN Code', value: '' },
+    { id: 5, label: 'Principal Name & Qualification', value: '' },
+    { id: 6, label: 'School Email ID', value: '' },
+    { id: 7, label: 'Contact Details (Landline/Mobile)', value: '' },
+  ];
+
+  classXResults = [
+    {
+      id: 1,
+      year: 2025,
+      registered: '',
+      passed: '',
+      percentage: '',
+      remarks: '',
+    },
+    {
+      id: 2,
+      year: 2024,
+      registered: '',
+      passed: '',
+      percentage: '',
+      remarks: '',
+    },
+    {
+      id: 3,
+      year: 2023,
+      registered: '',
+      passed: '',
+      percentage: '',
+      remarks: '',
+    },
+  ];
+
+  classXIIResults = [
+    {
+      id: 1,
+      year: 2025,
+      registered: '',
+      passed: '',
+      percentage: '',
+      remarks: '',
+    },
+    {
+      id: 2,
+      year: 2024,
+      registered: '',
+      passed: '',
+      percentage: '',
+      remarks: '',
+    },
+    {
+      id: 3,
+      year: 2023,
+      registered: '',
+      passed: '',
+      percentage: '',
+      remarks: '',
+    },
+  ];
+
+  staffInformation = [
+    { id: 1, label: 'Principal', value: '' },
+    { id: 2, label: 'Total No. of Teachers', value: '' },
+    { id: 3, label: 'PGT', value: '' },
+    { id: 4, label: 'TGT', value: '' },
+    { id: 5, label: 'PRT', value: '' },
+    { id: 6, label: 'Teachers Section Ratio', value: '' },
+    { id: 7, label: 'Details of Special Educator', value: '' },
+    {
+      id: 8,
+      label: 'Details of Counsellor and Wellness Teacher',
+      value: '',
+    },
+  ];
+
+  infrastructureInformation = [
+    {
+      id: 1,
+      label: 'Total Campus Area of the School (in Square Mtr)',
+      value: '',
+    },
+    {
+      id: 2,
+      label: 'No. and Size of the Class Rooms (in Square Mtr)',
+      value: '',
+    },
+    {
+      id: 3,
+      label:
+        'No. and Size of Laboratories Including Computer Labs (in Square Mtr)',
+      value: '',
+    },
+    {
+      id: 4,
+      label: 'Internet Facility (Yes/No)',
+      value: '',
+    },
+    {
+      id: 5,
+      label: 'No. of Girls Toilets',
+      value: '',
+    },
+    {
+      id: 6,
+      label: 'No. of Boys Toilets',
+      value: '',
+    },
+    {
+      id: 7,
+      label:
+        'Link of YouTube Video of the Inspection of School Covering the Infrastructure of the School',
+      value: '',
+    },
+  ];
 
   constructor(private readonly docsService: DocsService) {}
 
